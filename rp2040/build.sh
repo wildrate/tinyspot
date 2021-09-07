dcd6e2aa75606c7f0b667e5ebd56776529d374cb
Tim Lukins <tim@wildrate.org>
2021-09-07 22:11:45 +0100

    Got it working.

--- rp2040/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir build
-docker run --rm -v $PWD/src:/tmp/src -v $PWD/build:/tmp/build -w /tmp/build tinyspot-rp2040 cmake ../src
-#-DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release
-docker run --rm -v $PWD/src:/tmp/src -v $PWD/build:/tmp/build -w /tmp/build tinyspot-rp2040 make
-
